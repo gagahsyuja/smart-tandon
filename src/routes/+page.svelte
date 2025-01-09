98e3d509180ca474efa64140c1be0b654b836ccb
gagahsyuja <gghssa1@gmail.com>
2025-01-09 14:15:43 +0700

    Add todos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script>
     import Main from "$lib/main.svelte";
+    /* TODO:
+        - INPUTAN NOMOR UNTUK MENGATUR KAPAN KRAN TERBUKA BISA BERUPA JARAK (CM)
+        - INPUTAN NOMOR UNTUK MENDEFINISIKAN TINGGI TANDON (CM)
+        - WARNING KETIKA SUHU PANAS, MISALNYA DIATAS 30 DERAJAT
+    */
 </script>
 
-<Main />
+<Main temperature={120} />
