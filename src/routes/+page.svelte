13e420b18b607bcf040f50d0695c1a696cb8c13f
gagahsyuja <gghssa1@gmail.com>
2025-01-11 10:46:33 +0700

    Revert "Implement concurrently"
    This reverts commit dfb4ba684abb0f3357241805504a89bddb24a809.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+
+    import Main from "$lib/main.svelte";
+	import type { PageData } from './$types';
+
+    /* TODO:
+        - INPUTAN NOMOR UNTUK MENGATUR KAPAN KRAN TERBUKA BISA BERUPA JARAK (CM)
+        - INPUTAN NOMOR UNTUK MENDEFINISIKAN TINGGI TANDON (CM)
+        - WARNING KETIKA SUHU PANAS, MISALNYA DIATAS 30 DERAJAT
+    */
+
+	let { data }: { data: PageData } = $props();
+
+</script>
+
+<Main />
+
+<a href="/logs">Logs</a>
