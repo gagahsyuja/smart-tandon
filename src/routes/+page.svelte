d0bdd85929b8d86f6f591a551ad91f9c5d9b394b
gagahsyuja <gghssa1@gmail.com>
2025-01-11 10:36:48 +0700

    Add logs page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,18 @@
-<script>
+<script lang="ts">
+
     import Main from "$lib/main.svelte";
+	import type { PageData } from './$types';
+
     /* TODO:
         - INPUTAN NOMOR UNTUK MENGATUR KAPAN KRAN TERBUKA BISA BERUPA JARAK (CM)
         - INPUTAN NOMOR UNTUK MENDEFINISIKAN TINGGI TANDON (CM)
         - WARNING KETIKA SUHU PANAS, MISALNYA DIATAS 30 DERAJAT
     */
+
+	let { data }: { data: PageData } = $props();
+
 </script>
 
-<Main temperature={120} />
+<Main />
+
+<a href="/logs">Logs</a>
---
+++ src/routes/logs/+page.ts
@@ -0,0 +1,9 @@
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ fetch, params }) => {
+
+    let response = await fetch('http://127.0.0.1:3000/api/information');
+    let information = await response.json();
+
+    return { information };
+};
