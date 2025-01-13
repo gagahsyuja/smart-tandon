584231bf4a25eb1e38aea141224dd5cefe02a93c
gagahsyuja <gghssa1@gmail.com>
2025-01-14 00:53:00 +0700

    Change url

--- src/routes/logs/+page.ts
+++ src/routes/logs/+page.ts
@@ -2,7 +2,7 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch, params }) => {
 
-    let response = await fetch('http://127.0.0.1:3000/api/information');
+    let response = await fetch('https://doscom.org/api/information');
     let information = await response.json();
 
     return { information };
